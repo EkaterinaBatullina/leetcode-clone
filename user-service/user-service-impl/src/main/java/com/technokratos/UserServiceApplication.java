aa4415d813856650fb0b278f8e809db54ae0259a
EkaterinaBatullina <batullina2005@gmail.com>
2026-03-21 20:22:02 +0300

    refactor: clean up main class and update docs

--- user-service/user-service-impl/src/main/java/com/technokratos/UserServiceApplication.java
+++ user-service/user-service-impl/src/main/java/com/technokratos/UserServiceApplication.java
@@ -12,12 +12,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 public class UserServiceApplication {
     public static void main(String[] args) {
-        System.out.println((int) 'A');
-        System.out.println((int) 'Z');
-        System.out.println((int) 'a');
-        System.out.println((int) 'z');
-        System.out.println((int) '0');
-        System.out.println((int) '9');
-        //SpringApplication.run(UserServiceApplication.class, args);
+        SpringApplication.run(UserServiceApplication.class, args);
     }
 }
